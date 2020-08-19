17ddf0449c11c413222998a280a77a3fe070719b
Pablo Corbalán De Concepción <63709477+PabloCorbCon@users.noreply.github.com>
2020-08-19 09:26:51 +0200

    main file created

---
+++ bot/app.py
@@ -0,0 +1,4 @@
+# This is the main file of Taur. Our Discord bot written in discord.py
+# if you need more information about Taur, you can check the GitHub repository
+# https://github.com/PabloCorCon/Taur
+
