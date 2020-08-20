dd121d52f3dd7328bfb56cbeacb7c2ec03ebaf59
Pablo Corbalán De Concepción <63709477+PabloCorbCon@users.noreply.github.com>
2020-08-20 21:48:03 +0200

    t/info implemented

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
-# Taur
-
-
-Taur is a Discord bot written with the discord.py library that implements a large number of commands, although it is
-still in the early stages of development. Every week we add three new commands to Taur, which are previously tested.
-
-**Invite Taur to your server [here](https://discord.com/oauth2/authorize?client_id=745535486784831509&scope=bot&per:
-prin    
-
-github_REPO = 'github.com/PabloCorbCon/Taur
-print('\€{}).format(gith
-our ))NzQ1NTM1NDg2Nzg0ODMxNTA5.XzzMBw.r003GcH0Wd5mKxAL5H2wwUP4dw0
-
 
 # Taur
 
@@ -18,13 +5,9 @@ our ))NzQ1NTM1NDg2Nzg0ODMxNTA5.XzzMBw.r003GcH0Wd5mKxAL5H2wwUP4dw0
 Taur is a Discord bot written with the discord.py library that implements a large number of commands, although it is
 still in the early stages of development. Every week we add three new commands to Taur, which are previously tested.
 
-**Invite Taur to your server [here](https://discord.com/oauth2/authorize?client_id=745535486784831509&scope=bot&per:
-prin    
+**Invite Taur to your server [here](https://discord.com/oauth2/authorize?client_id=745535486784831509&scope=bot&per:)
 
-github_REPO = 'github.com/PabloCorbCon/Taur
-print('\€{}).format(gith
-our ))NzQ1NTM1NDg2Nzg0ODMxNTA5.XzzMBw.r003GcH0Wd5mKxAL5H2wwUP4dw0
-
+github_REPO = 'github.com/PabloCorbCon/Taur'
 
 # Taur
 
--- bot/app.py
+++ bot/app.py
@@ -3,20 +3,18 @@
 # https://github.com/PabloCorCon/Taur
 
 import discord
+from discord.ext import commands
 import colorama
 import platform
 import json
 
 colorama.init(autoreset=True)
 
-# we create the discord client.
-with open('doc/description.txt') as description:
+TOKEN = 'NzQ1NTM1NDg2Nzg0ODMxNTA5.XzzMBw.kQQS0wNEQmUOpmf0QlMSxG7pzDo'
 
-    d = description.read()
+bot = commands.Bot(command_prefix='t/')
 
-client = discord.Client(description=d)
-
-@client.event
+@bot.event
 async def on_ready():
 
     """
@@ -29,10 +27,10 @@ async def on_ready():
     print('   GitHub: @PabloCorbCon')
 
     # bot log in information
-    print('\nLogged in as {}, id: {} | Servers: {} | Users: {}'.format(client.user.name,
-                                                                     client.user.id,
-                                                                     len(client.guilds),
-                                                                     len(set(client.get_all_members()))) + ' users')
+    print('\nLogged in as {}, id: {} | Servers: {} | Users: {}'.format(bot.user.name,
+                                                                     bot.user.id,
+                                                                     len(bot.guilds),
+                                                                     len(set(bot.get_all_members()))) + ' users')
 
     # bot python information
     print('\nPython version: {} | Discord.py version: {}'.format(platform.python_version(),
@@ -40,11 +38,47 @@ async def on_ready():
 
     #liks
     invite = 'https://discord.com/oauth2/authorize?client_id=745535486784831509&scope=bot&permissions=268690782'
-    print('\nUse this link to invite {}:'.format(client.user.name))
+    print('\nUse this link to invite {}:'.format(bot.user.name))
     print(invite)
 
     github_repo = 'github.com/PabloCorbCon/Taur'
     print('\nGitHub repository: {}'.format(github_repo))
 
 
-client.run()
+@bot.command
+async def info():
+
+    """
+    This function will display information about the bot using the
+    t/info ccommand, to display an embed message.
+    """
+    # description of the message
+    d = '''
+Taur is a Discord bot written with the discord.py library that implements a large number of commands,
+although it is still in the early stages of development.
+Every week we add at least three new commands to Taur, which are previously tested.
+
+Maybe you need the command **t/commands**, to display the bot commands.
+    '''
+
+    # create the discord embed message
+    info_embed = discord.Embed(title="Information",
+        description=d,
+        color=0x0a8f3f)
+
+    with open('doc/description.txt', 'r') as f:
+
+        info_description = f.read()
+
+    # create the embed message
+    info_embed=discord.Embed(title="Taur | Information",
+        description=info_description,
+        color=0x087d1b)
+    info_embed.set_author(name="Taur",
+        url="https://github.com/PabloCorbCon/Taur")
+    info_embed.set_image('https://github.com/PabloCorbCon/Taur/blob/master/branding/logo.png')
+    info_embed.set_footer(text="By Pablo Corbalán | Twitter: @pablocorbcon - GitHub: @PabloCorbCon")
+    
+    await bot.say(embed=info_embed)
+
+bot.run(TOKEN)
