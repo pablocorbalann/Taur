5bf42b78c46c0d248eaf8b8d7bfe756fd5dd72c0
Pablo Corbalán De Concepción <63709477+PabloCorbCon@users.noreply.github.com>
2020-09-02 00:23:41 +0200

    sended_to() function created inside the bot/decorators.py file

--- bot/decorators.py
+++ bot/decorators.py
@@ -15,4 +15,26 @@ def add_reactions(msg, reactions = ()):
 		msg.add_reaction(reaction)
 
 def responded_to(command):
+
+	"""
+	This public function is used as a debug tool and server side decorator that 
+	will print in the python console the following message:
+
+		Taur has responded to a command (t/command).
+
+	Using the colorame.Fore module to print it in magenta
+	"""
 	return colorama.Fore.LIGHTMAGENTA_EX + 'Taur has responded to a command ({0}).'.format(command)
+
+def sended_to(user):
+
+	"""
+	This public function is used as a debug tool and server side decorator that 
+	will print in the python console the following message:
+
+		Taur has sended a private message to: user.
+
+	Using the colorame.Fore module to print it in cyan
+	"""
+
+	return colorama.Fore.LIGHTMAGENTA_EX + 'Taur has sended a private message to: {0}.'.format(user)
