6ac32fed0509565037c7bff482da977e70b98138
Pablo Corbalán De Concepción <63709477+PabloCorbCon@users.noreply.github.com>
2020-09-01 10:15:16 +0200

    implementation finished

--- bot/app.py
+++ bot/app.py
@@ -23,7 +23,7 @@ yellow = colorama.Fore.LIGHTYELLOW_EX
 violet = colorama.Fore.LIGHTMAGENTA_EX
 
 # set the token (const token)
-TOKEN = 
+TOKEN = 'NzQ1NTM1NDg2Nzg0ODMxNTA5.XzzMBw.Ct2aSWis9QgSl21p6avYqGsY51o'
 
 # create the bot using the discord.Bot() class
 bot = commands.Bot(command_prefix='t/')
